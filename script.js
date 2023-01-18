fbe1dc10ffd09f71fe81b3ffcbd4d2cf193d13b6
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2023-01-18 23:12:45 +0530

    Implement game function

--- script.js
+++ script.js
@@ -32,3 +32,26 @@ function playRound(playerSelection, computerSelection) {
         }`;
     else return "It's a tie!";
 }
+
+function game() {
+    let playerScore = 0,
+        computerScore = 0;
+    for (let index = 0; index < 5; index++) {
+        const playerSelection = prompt(
+            "Choose any one: (Rock, Paper, Scissors)"
+        );
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        if (result.includes("Won")) {
+            playerScore++;
+        } else if (result.includes("Lose")) {
+            computerScore++;
+        }
+        console.log(result);
+    }
+    if (playerScore > computerScore) {
+        console.log("Congrats! You won the game.");
+    } else {
+        console.log("Oops! You lose.");
+    }
+}
